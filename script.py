79e87a86401f5a26a10bb084a8eb1f5df7cf3911
admirallllllll <42350536+admirallllllll@users.noreply.github.com>
2020-09-23 19:47:17 +0000

    Update script.py

--- script.py
+++ script.py
@@ -4,7 +4,7 @@ file = os.stat("dz1.py")
 parser = argparse.ArgumentParser()
 parser.add_argument("-m", "--mtime", help="time", action = "store_true")
 parser.add_argument("-s", "--size", help="size", action = "store_true")
-parser.add_argument("--rename", help="rename", action="store_true")
+parser.add_argument("--rename", help="rename")
 args = parser.parse_args()
 mtime = args.mtime
 size = args.size
@@ -12,6 +12,5 @@ rename = args.rename
 if mtime:
     print("last modified: %s" % time.ctime(st.st_mtime))
 if size:
-    print("size: %s" %file.st_size)
-if rename:
-    os.rename('dz1.py', 'B.py')
+    print("size: %s" % (file.st_size / 2 ** 10))
+os.rename('dz1.py', str(rename))
